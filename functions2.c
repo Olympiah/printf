5a9cb1c9c0f7d40a1cd3d73f61c691cad5359635
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-10-20 02:53:50 -0700

    get flag

--- functions2.c
+++ functions2.c
@@ -19,36 +19,36 @@ int print_pointer(va_list types, char buffer[],
 	unsigned long num_addrs;
 	char map_to[] = "0123456789abcdef";
 	void *addrs = va_arg(types, void *);
-	
+
 	UNUSED(width);
 	UNUSED(size);
-	
+
 	if (addrs == NULL)
 		return (write(1, "(nil)", 5));
-	
+
 	buffer[BUFF_SIZE - 1] = '\0';
 	UNUSED(precision);
-	
+
 	num_addrs = (unsigned long)addrs;
-	
+
 	while (num_addrs > 0)
 	{
 		buffer[ind--] = map_to[num_addrs % 16];
 		num_addrs /= 16;
 		length++;
 	}
-	
+
 	if ((flags & F_ZERO) && !(flags & F_MINUS))
 		padd = '0';
-	
+
 	if (flags & F_PLUS)
 		extra_c = '+', length++;
-	
+
 	else if (flags & F_SPACE)
 		extra_c = ' ', length++;
-	
+
 	ind++;
-	
+
 	return (write_pointer(buffer, ind, length,
 		width, flags, padd, extra_c, padd_start));
 }
@@ -70,27 +70,27 @@ int print_non_printable(va_list ap, char buffer[],
 {
 	int i = 0, offset = 0;
 	char *str = va_arg(types, char *);
-	
+
 	UNUSED(flags);
 	UNUSED(width);
 	UNUSED(precision);
 	UNUSED(size);
-	
+
 	if (str == NULL)
 		return (write(1, "(null)", 6));
-	
+
 	while (str[i] != '\0')
 	{
 		if (is_printable(str[i]))
 			buffer[i + offset] = str[i];
 		else
 			offset += append_hexa_code(str[i], buffer, i + offset);
-		
+
 		i++;
 	}
-	
+
 	buffer[i + offset] = '\0';
-	
+
 	return (write(1, buffer, i + offset));
 }
 
@@ -114,17 +114,17 @@ int print_rot13string(va_list ap, char buffer[],
 	int count = 0;
 	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	
+
 	str = va_arg(types, char *);
 	UNUSED(buffer);
 	UNUSED(flags);
 	UNUSED(width);
 	UNUSED(precision);
 	UNUSED(size);
-	
+
 	if (str == NULL)
 		str = "(AHYY)";
-	
+
 	for (i = 0; str[i]; i++)
 	{
 		for (j = 0; in[j]; j++)
@@ -144,7 +144,7 @@ int print_rot13string(va_list ap, char buffer[],
 			count++;
 		}
 	}
-	
+
 	return (count);
 }
 
