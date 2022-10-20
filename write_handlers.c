4fdc83ebc087111c42a829c0afd77229d93d3daa
Olympiah Otieno <87874970+Olympiah@users.noreply.github.com>
2022-10-20 21:27:58 +0300

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -106,36 +106,30 @@ int write_num(int ind, char buffer[],
 		return (0); /* printf(".0d", 0)  no char is printed */
 	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
 		buffer[ind] = padd = ' '; /* width is displayed with padding ' ' */
-
 	if (prec > 0 && prec < length)
 		padd = ' ';
-
+	
 	while (prec > length)
 		buffer[--ind] = '0', length++;
-
 	if (extra_c != 0)
 		length++;
-
 	if (width > length)
 	{
 		for (i = 1; i < width - length + 1; i++)
 			buffer[i] = padd;
 		buffer[i] = '\0';
-
 		if (flags & F_MINUS && padd == ' ')/* Asign extra char to left of buffer */
 		{
 			if (extra_c)
 				buffer[--ind] = extra_c;
 			return (write(1, &buffer[ind], length) + write(1, &buffer[1], i - 1));
 		}
-
 		else if (!(flags & F_MINUS) && padd == ' ')/* extra char to left of buff */
 		{
 			if (extra_c)
 				buffer[--ind] = extra_c;
 			return (write(1, &buffer[1], i - 1) + write(1, &buffer[ind], length));
 		}
-
 		else if (!(flags & F_MINUS) && padd == '0')/* extra char to left of padd */
 		{
 			if (extra_c)
@@ -144,7 +138,6 @@ int write_num(int ind, char buffer[],
 					write(1, &buffer[ind], length - (1 - padd_start)));
 		}
 	}
-
 	if (extra_c)
 		buffer[--ind] = extra_c;
 	return (write(1, &buffer[ind], length));
