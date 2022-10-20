902cc8ca0652bed10c85db74b7e6d097bc9dfe80
Olympiah Otieno <87874970+Olympiah@users.noreply.github.com>
2022-10-20 20:55:37 +0300

    Update functions2.c

--- functions2.c
+++ functions2.c
@@ -105,6 +105,49 @@ int print_non_printable(va_list ap, char buffer[],
  * Return: Numbers of chars printed
  */
 
+int print_reverse(va_list ap, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	char *str;
+	int i, count = 0;
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(size);
+
+	str = va_arg(ap, char *);
+
+	if (str == NULL)
+	{
+		UNUSED(precision);
+
+		str = ")Null(";
+	}
+	for (i = 0; str[i]; i++)
+		;
+
+	for (i = i - 1; i >= 0; i--)
+	{
+		char z = str[i];
+
+		write(1, &z, 1);
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * print_rot13string - Print a string in rot13.
+ * @ap: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Numbers of chars printed
+ */
+
 int print_rot13string(va_list ap, char buffer[],
 	int flags, int width, int precision, int size)
 {
