8975636e864e080ac2096ad224962893ceda9a39
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-10-20 09:26:44 -0700

    fix main.h

--- main.h
+++ main.h
@@ -53,7 +53,7 @@ va_list ap, char buffer[], int flags, int width, int precision, int size);
 
 /* Funtions to print chars and strings */
 
-int print_c(va_list ap, char buffer[],
+int print_char(va_list ap, char buffer[],
 	int flags, int width, int precision, int size);
 int print_string(va_list ap, char buffer[],
 	int flags, int width, int precision, int size);
@@ -72,40 +72,40 @@ int print_octal(va_list ap, char buffer[],
 	int flags, int width, int precision, int size);
 int print_hexadecimal(va_list ap, char buffer[],
 	int flags, int width, int precision, int size);
-int print_hexa_upper(va_listap, char buffer[],
+int print_hexa_upper(va_list ap, char buffer[],
 	int flags, int width, int precision, int size);
 
 
-int print_hexa(va_list types, char map_to[],
+int print_hexa(va_list ap, char map_to[],
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
 /* Function to print non printable characters */
-int print_non_printable(va_list types, char buffer[],
+int print_non_printable(va_list ap, char buffer[],
 	int flags, int width, int precision, int size);
 
 
 /* Function to print memory address */
-int print_pointer(va_list types, char buffer[],
+int print_pointer(va_list ap, char buffer[],
 	int flags, int width, int precision, int size);
 
 
 /* Funciotns to handle other specifiers */
 
 int get_flags(const char *format, int *i);
-int get_width(const char *format, int *i, va_list list);
-int get_precision(const char *format, int *i, va_list list);
+int get_width(const char *format, int *i, va_list ap);
+int get_precision(const char *format, int *i, va_list ap);
 int get_size(const char *format, int *i);
 
 
 /*Function to print string in reverse*/
 
-int print_reverse(va_list types, char buffer[],
+int print_reverse(va_list ap, char buffer[],
 	int flags, int width, int precision, int size);
 
 
 /*Function to print a string in rot 13*/
 
-int print_rot13string(va_list types, char buffer[],
+int print_rot13string(va_list ap, char buffer[],
 	int flags, int width, int precision, int size);
 
 
